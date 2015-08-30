3ac231edfd2e59bf4435edafb0f8464cdfb135d9
cswiger <cswiger@gmail.com>
2015-08-30 15:08:08 -0400

    Update createPage.py
    correct table layout

--- createPage.py
+++ createPage.py
@@ -4,7 +4,7 @@ from datetime import datetime
 def createPage(jfc):
   page = "<html><head/><body><table border=1><tr><td>Date</td><td>Precip Probability</td></tr>"
   for idx in range(len(jfc["hourly"]["data"])):
-     page += "<tr><td>%s</td><td>%s</td></tr>" % (jfc["hourly"]["data"][idx]["precipProbability"],datetime.fromtimestamp(jfc["hourly"]["data"][idx]["time"]).strftime('%Y-%m-%d %H:%M:%S') )
+     page += "<tr><td>%s</td><td>%s</td></tr>" % ( datetime.fromtimestamp(jfc["hourly"]["data"][idx]["time"]).strftime('%Y-%m-%d %H:%M:%S'),jfc["hourly"]["data"][idx]["precipProbability"] )
   page += "</body></html>"
   return bytes(page,'utf-8')
 
